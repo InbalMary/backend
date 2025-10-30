56ef9a62fa3cd1fe4a4284969c4c1d6896fe4f9e
InbalMary <inbalby10@gmail.com>
2025-10-30 15:18:03 +0200

    Add public folder

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: process.env.MONGO_URL,
+    dbURL: process.env.MONGO_URL || 'mongodb+srv://inbal:123@cluster0.mamqapt.mongodb.net/?appName=Cluster0',
     dbName: process.env.DB_NAME || 'stay_db'
 }
